1138008f99eacf4fef34c8489d340d2da2e95892
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2024-03-01 11:03:50 +0000

    fix logger lib

---
+++ server/libraries/logger-lib/errorLevels.js
@@ -0,0 +1,5 @@
+export const ERROR_LEVEL = {
+  INFO: 0,
+  ERROR: 1,
+  FATAL: 2,
+};
---
+++ server/libraries/logger-lib/logger.js
@@ -0,0 +1,15 @@
+import winston, { format } from "winston";
+import dotenvFlow from "dotenv-flow";
+dotenvFlow.config();
+export const logger = winston.createLogger({
+  level: "debug",
+  format: format.combine(
+    format.timestamp({
+      format: "MM-DD-YYYY HH:mm:ss",
+    }),
+    format.json()
+  ),
+  transports: [new winston.transports.Console()],
+});
+
+export default logger;
