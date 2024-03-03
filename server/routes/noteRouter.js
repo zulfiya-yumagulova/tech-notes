f8bb7c41130a9fa234c64b90ca320bf3b8657ea7
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2024-03-03 13:13:42 +0000

    add note routes

--- server/routes/noteRouter.js
+++ server/routes/noteRouter.js
@@ -1,7 +1,17 @@
 import express from "express";
-import { getAllNotes } from "../controllers/noteController.js";
+import {
+  getAllNotes,
+  getNote,
+  createNote,
+  updateNote,
+  deleteNote,
+} from "../controllers/noteController.js";
 
 const app = express();
 export const noteRouter = express.Router();
 
 noteRouter.get("/", getAllNotes);
+noteRouter.get("/:id", getNote);
+noteRouter.post("/", createNote);
+noteRouter.patch("/", updateNote);
+noteRouter.delete("/", deleteNote);
